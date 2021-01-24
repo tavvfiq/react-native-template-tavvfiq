b10fa7e74feb4fd7d1e8ea76cfeadb9b59cfb584
tavvfiq <taufiq.w@mail.ugm.ac.id>
2021-01-24 14:08:18 +0700

    chore: init repo

---
+++ src/styles/colors.ts
@@ -0,0 +1,19 @@
+export const PRIMARY = "#6379F4";
+export const SECONDARY = "#767676";
+export const WHITE = "#FFFFFF";
+export const BLACK = "#000000";
+export const TEXT_COLOR_PRIMARY = "rgb(3,23,9)";
+export const TEXT_COLOR_SECONDARY = "rgb(150,150,150)";
+export const CARD_TITLE_BG = "#f89f1e";
+
+// ACTIONS
+export const SUCCESS = "#3adb76";
+export const WARNING = "#ffae00";
+export const ALERT = "#cc4b37";
+export const DOABLE = "#e71d35";
+
+// GRAYSCALE
+export const GRAY_LIGHT = "#e6e6e6";
+export const GRAY_MEDIUM = "#cacaca";
+export const GRAY_DARK = "#8a8a8a";
+export const GRAY = "rgba(240,240,240)";
---
+++ src/styles/typography.ts
@@ -0,0 +1,36 @@
+import {scaleFont} from './mixins';
+
+// FONT FAMILY
+export const FONT_FAMILY_REGULAR = 'Poppins-Regular';
+export const FONT_FAMILY_MEDIUM = 'Poppins-Medium';
+export const FONT_FAMILY_BOLD = 'Poppins-Bold';
+
+// FONT WEIGHT
+export const FONT_WEIGHT_REGULAR = '400';
+export const FONT_WEIGHT_BOLD = '700';
+
+// FONT SIZE
+export const FONT_SIZE_24 = scaleFont(24);
+export const FONT_SIZE_20 = scaleFont(20);
+export const FONT_SIZE_16 = scaleFont(16);
+export const FONT_SIZE_14 = scaleFont(14);
+export const FONT_SIZE_12 = scaleFont(12);
+export const FONT_SIZE_11 = scaleFont(11);
+export const FONT_SIZE_9 = scaleFont(9);
+
+// LINE HEIGHT
+export const LINE_HEIGHT_24 = scaleFont(24);
+export const LINE_HEIGHT_20 = scaleFont(20);
+export const LINE_HEIGHT_16 = scaleFont(16);
+export const LINE_HEIGHT_13 = scaleFont(13);
+
+// FONT STYLE
+export const FONT_REGULAR = {
+  fontFamily: FONT_FAMILY_REGULAR,
+  // fontWeight: FONT_WEIGHT_REGULAR,
+};
+
+export const FONT_BOLD = {
+  fontFamily: FONT_FAMILY_BOLD,
+  // fontWeight: FONT_WEIGHT_BOLD,
+};
